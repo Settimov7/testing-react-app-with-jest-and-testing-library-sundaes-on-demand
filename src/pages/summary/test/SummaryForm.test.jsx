10e2af42c4407a571fef6963aad53dbd2e4b200f
Settimo <prytkov.nikita.set@gmail.com>
2021-10-26 10:04:53 +0400

    add code and test for popover

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,10 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { SummaryForm } from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("confirm order button is disabled if checkbox is unchecked", () => {
   render(<SummaryForm />);
@@ -25,13 +30,42 @@ test("confirm order button is enabled after click on checkbox and returned back 
     name: /confirm order/i,
   });
 
-  fireEvent.click(checkboxElement);
+  userEvent.click(checkboxElement);
 
   expect(checkboxElement).toBeChecked();
   expect(confirmOrderButtonElement).toBeEnabled();
 
-  fireEvent.click(checkboxElement);
+  userEvent.click(checkboxElement);
 
   expect(checkboxElement).not.toBeChecked();
   expect(confirmOrderButtonElement).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+
+  let popoverElement = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  const termsAndConditionsElement = screen.getByText(/terms and conditions/i);
+
+  expect(popoverElement).not.toBeInTheDocument();
+
+  userEvent.hover(termsAndConditionsElement);
+
+  popoverElement = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popoverElement).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditionsElement);
+
+  await waitForElementToBeRemoved(() => {
+    popoverElement = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+
+    return popoverElement;
+  });
+
+  expect(popoverElement).not.toBeInTheDocument();
 });
